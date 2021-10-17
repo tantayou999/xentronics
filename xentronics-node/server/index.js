475cf8f604c81375785a7043e01bb1677a86b1e7
Edward Tan <tantayou999@gmail.com>
2021-10-17 22:22:14 +0800

    Unit test with Mocha and Chai. Tooling with eslint, nyc and git-pre-push

--- xentronics-node/server/index.js
+++ xentronics-node/server/index.js
@@ -1,33 +1,33 @@
 // server/index.js
 
 const express = require("express");
-const db = require('./queries')
 
-var cors = require('cors')
+const cors = require("cors");
 
-var bodyParser = require('body-parser')
- 
+const bodyParser = require("body-parser");
+const db = require("./queries");
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+const server = app.listen(PORT, () => {
+  // console.log(`Server listening on ${PORT}`);
 });
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from Express!" });
-  });
+  res.json({ message: "Hello from Express!" });
+});
 
-app.get('/api/users', db.getUsers)
-app.get('/api/products/categories', db.getProductCategories)
-app.get('/api/products/:product_id', db.getProductsById)
-app.get('/api/products/categories/:category_id', db.getProducts)
-app.get('/api/users/:user_id/cart', db.getCartById)
-app.post('/api/users/:user_id/cart', db.addToCart)
-app.delete('/api/users/:user_id/cart', db.removeFromCart)
-app.get('/api/users/:user_id/cart/checkout', db.checkOut)
-app.get('/api/users/:user_id', db.getUser)
+app.get("/api/products/categories", db.getProductCategories);
+app.get("/api/products/:product_id", db.getProductsById);
+app.get("/api/products/categories/:category_id", db.getProducts);
+app.get("/api/users/:user_id/cart", db.getCartById);
+app.post("/api/users/:user_id/cart", db.addToCart);
+app.delete("/api/users/:user_id/cart", db.removeFromCart);
+app.get("/api/users/:user_id/cart/checkout", db.checkOut);
+app.get("/api/users/:user_id", db.getUser);
 
+module.exports = server;
--- xentronics-node/server/queries.js
+++ xentronics-node/server/queries.js
@@ -1,108 +1,137 @@
-const Pool = require('pg').Pool
-const config = require('./config.js')
-const pool = new Pool(config.db)
+const { Pool } = require("pg");
+const config = require("./config.json");
 
-const getUsers = (request, response) => {
-pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-    throw error
-    }
-    response.status(200).json(results.rows)
-})
-}
+const pool = new Pool(config.db);
 
 const getProductCategories = (request, response) => {
-    pool.query('SELECT * FROM productcategories ORDER BY id ASC', (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  pool.query(
+    "SELECT * FROM productcategories ORDER BY id ASC",
+    (error, results) => {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const getProducts = (request, response) => {
-    const category_id = parseInt(request.params.category_id)
-    pool.query('SELECT * FROM products WHERE category_id = $1 ORDER BY id ASC', [category_id], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  const { category_id } = request.params;
+  pool.query(
+    "SELECT * FROM products WHERE category_id = $1 ORDER BY id ASC",
+    [category_id],
+    (error, results) => {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const getProductsById = (request, response) => {
-    const product_id = parseInt(request.params.product_id)
-    pool.query('SELECT * FROM products WHERE id = $1 ORDER BY id ASC', [product_id], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  const { product_id } = request.params;
+  pool.query(
+    "SELECT * FROM products WHERE id = $1 ORDER BY id ASC",
+    [product_id],
+    (error, results) => {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const getCartById = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
-  
-    pool.query('SELECT * FROM productsincart LEFT JOIN (SELECT * FROM products) p ON (productsincart.product_id = p.id) WHERE user_id = $1', [user_id], (error, results) => {
+  const { user_id } = request.params;
+
+  pool.query(
+    "SELECT * FROM productsincart LEFT JOIN (SELECT * FROM products) p ON (productsincart.product_id = p.id) WHERE user_id = $1",
+    [user_id],
+    (error, results) => {
       if (error) {
-        throw error
+        response.status(500).send(error);
+        return;
       }
-      response.status(200).json(results.rows)
-    })
-}
+      response.status(200).json(results.rows);
+    }
+  );
+};
 
 const addToCart = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
-    const product_id = parseInt(request.body.product_id)
-    const qty = parseInt(request.body.qty)
-    pool.query('INSERT INTO productsincart(product_id, user_id, qty) VALUES ($1,$2,$3) ON CONFLICT (product_id, user_id) DO UPDATE SET qty=$3;', [product_id, user_id, qty], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).send(`Added: ${qty} PRODUCT ${product_id} to ${user_id}'s cart'`)
-    })
-}
-
+  const { user_id } = request.params;
+  const { product_id } = request.body;
+  const { qty } = request.body;
+  pool.query(
+    "INSERT INTO productsincart(product_id, user_id, qty) VALUES ($1,$2,$3) ON CONFLICT (product_id, user_id) DO UPDATE SET qty=$3;",
+    [product_id, user_id, qty],
+    (error) => {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      }
+      response
+        .status(200)
+        .send(`Added: ${qty} PRODUCT ${product_id} to ${user_id}'s cart`);
+    }
+  );
+};
 
 const removeFromCart = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
-    const product_id = parseInt(request.body.product_id)
+  const { user_id } = request.params;
+  const { product_id } = request.body;
 
-    pool.query('DELETE FROM productsincart WHERE user_id = $1 AND product_id = $2', [user_id, product_id], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).send(`Deleted: PRODUCT ${product_id} to ${user_id}'s cart`)
-    })
-}
+  pool.query(
+    "DELETE FROM productsincart WHERE user_id = $1 AND product_id = $2",
+    [user_id, product_id],
+    (error) => {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      }
+      response
+        .status(200)
+        .send(`Deleted: PRODUCT ${product_id} in ${user_id}'s cart`);
+    }
+  );
+};
 
 const checkOut = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
-    pool.query('CALL checkout($1)', [user_id], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).send(`User deleted with ID: ${id}`)
-    })
-}
+  const { user_id } = request.params;
+  pool.query("CALL checkout($1)", [user_id], (error) => {
+    if (error) {
+      response.status(500).send(error);
+      return;
+    }
+    response.status(200).send(`User ${user_id} checked out`);
+  });
+};
 
 const getUser = (request, response) => {
-    const user_id = parseInt(request.params.user_id)
-    pool.query('SELECT * FROM productspurchased WHERE user_id = $1 ORDER BY id ASC', [user_id], (error, results) => {
-        if (error) {
-        throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  const { user_id } = request.params;
+  pool.query(
+    "SELECT * FROM productspurchased WHERE user_id = $1 ORDER BY id ASC",
+    [user_id],
+    (error, results) => {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
 module.exports = {
-    getUsers,
-    getProductCategories,
-    getProducts,
-    getProductsById,
-    getCartById,
-    addToCart,
-    removeFromCart,
-    checkOut,
-    getUser
-}
+  getProductCategories,
+  getProducts,
+  getProductsById,
+  getCartById,
+  addToCart,
+  removeFromCart,
+  checkOut,
+  getUser,
+};
